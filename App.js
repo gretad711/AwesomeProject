a06bf2d038dd9323ab05f66786868736c2497366
Greta Dakers <gretad711@msn.com>
2020-12-27 20:19:10 -0600

    total amount for all gigs working

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -14,12 +14,17 @@ import Todo from './Todo';
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
+  const [total, setTotal] = useState(0);
   const [gigs, setGigs] = useState([
     {
       description: 'Freelance job with Qazi',
       amount: 499.99,
     },
   ]);
+
+  useEffect(() => {
+    setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
+  }, [gigs]);
 
   const addGig = () => {
     setGigs([
@@ -39,10 +44,11 @@ const App = () => {
         <Text style={styles.firstLabel}>Hello World! 💫</Text>
       </View>
 
+      <Text>Total Income: ${total}</Text>
+
       <TextInput
         style={styles.input}
         value={description}
-        keyboardType="numeric"
         placeholder="Enter a description"
         onChangeText={(text) => setDescription(text)}
       />
@@ -54,14 +60,26 @@ const App = () => {
         placeholder="Enter the amount you made in USD ($)"
         onChangeText={(text) => setAmount(text)}
       />
-      <Button onPress={addGig} title="Add Gig 🚀" />
+      <Button
+        disabled={!amount && !description}
+        onPress={addGig}
+        title="Add Gig 🚀"
+      />
+
+      {gigs.map((gig) => (
+        <View>
+          <Text>{gig.description}</Text>
+          <Text>{gig.amount}</Text>
+        </View>
+      ))}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    margin: 20,
+    marginTop: 10,
+    marginBottom: 20,
     padding: 20,
     height: 40,
     borderColor: 'red',
