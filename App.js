b7a09a7fd49596c89f3d89851894a1613184062e
Greta Dakers <gretad711@msn.com>
2020-12-28 02:27:53 -0600

    update README.md file

--- App.js
+++ App.js
@@ -11,8 +11,6 @@ import {
 import { LineChart } from 'react-native-chart-kit';
 import moment from 'moment';
 
-// dataset needs to be in a state (variable)
-
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
@@ -33,38 +31,15 @@ const App = () => {
   ]);
   const [transformedData, setTransformedData] = useState([]);
 
-  // Every time data changes, this useEffect hook will run.
   useEffect(() => {
     setTransformedData(transformData(groupBy(data, 'date')));
   }, [data]);
-
-  // Below is what we get when we use the groupBy function
-  // [
-  // { [moment()]: 2000 },
-  // { [moment().subtract(1, 'days')]: 9500 },
-  // { [moment().subtract(2, 'days')]: 11000 },
-  // ]
 
   const groupBy = (array, key) =>
     array.reduce((rv, x) => {
       (rv[x[key]] = rv[x[key]] || []).push(x);
       return rv;
     }, {});
-  // example of what the code above does, in the example, it groups by the number of letters
-  // console.log(groupBy(['one', 'two', 'three'], 'length));
-  // => {3: ["one", "two"], 5: ["three"]}
-
-  // The output currently looks like the code below
-  //   The GROUPED values are--->
-  // {December 27, 2020: Array(1), December 26, 2020: Array(2), December 25, 2020: Array(2)}
-  // December 25, 2020: (2) [{…}, {…}]
-  // December 26, 2020: (2) [{…}, {…}]
-  // December 27, 2020: [{…}]
-
-  // We want the code to look like this
-  // December 25, 2020: 2000
-  // December 26, 2020: 9500
-  // December 27, 2020: 4000
 
   const [gigs, setGigs] = useState([
     {
@@ -75,15 +50,11 @@ const App = () => {
   ]);
 
   const getDates = () => transformedData.map((pair) => pair.date);
-  // what we want ---> [date1, date2, date3, date4, date5]
-  // we're using the 0 so that we get '12/28/2020'. The date is the first value in the array. ['12/28/2020'] ---> '12/28/2020'. If we didn't use [0], we would get back an array of arrays
   const getAmounts = () => transformedData.map((pair) => pair.amount);
-  // Create a new array with the date as the key and value as the sum of the amounts for each date (if it's a grouped date, meaning multiple amounts for a single date, we should see the sum of all of the amounts for that day)
   const transformData = (groupedData) => {
     const transformedArray = [];
 
     Object.entries(groupedData).forEach((entry) => {
-      // Entry[1] is an array of objects that holds all of the amounts (entry[0] holds the date).
       const total = entry[1].reduce((total, pair) => total + pair.amount, 0);
       transformedArray.push({
         date: moment(entry[0]).format('MM/DD'),
@@ -96,18 +67,6 @@ const App = () => {
 
     return sortedArray;
   };
-
-  console.log('DEBUG-->', data);
-  console.log('Dates--->', getDates());
-  console.log('Amounts--->', getAmounts());
-  console.log(
-    'The GROUPED values are--->',
-    Object.entries(groupBy(data, 'date'))
-  );
-  console.log(
-    'The Total grouped value 👽--->',
-    transformData(groupBy(data, 'date'))
-  );
 
   useEffect(() => {
     setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
@@ -151,10 +110,10 @@ const App = () => {
               },
             ],
           }}
-          width={Dimensions.get('window').width} // from react-native
+          width={Dimensions.get('window').width}
           height={220}
           yAxisLabel="$"
-          yAxisInterval={1} // optional, defaults to 1
+          yAxisInterval={1}
           chartConfig={{
             backgroundColor: '#e26a00',
             backgroundGradientFrom: '#fb8c00',
