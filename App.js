b3942c8aaf8478d7125a7e39d5e65c9d1c1d0430
Greta Dakers <gretad711@msn.com>
2020-12-28 00:31:41 -0600

    displays total dollar amount earned for each day

--- App.js
+++ App.js
@@ -7,20 +7,94 @@ import {
   SafeAreaView,
   TextInput,
   Button,
+  Dimensions,
   ScrollView,
 } from 'react-native';
-import Todo from './Todo';
+import { LineChart } from 'react-native-chart-kit';
+import moment from 'moment';
+
+// dataset needs to be in a state (variable)
 
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
   const [total, setTotal] = useState(0);
+  const [data, setData] = useState([
+    { date: moment().format('LL'), amount: 2000 },
+    { date: moment().subtract(1, 'days').format('LL'), amount: 2500 },
+    { date: moment().subtract(1, 'days').format('LL'), amount: 3500 },
+    { date: moment().subtract(2, 'days').format('LL'), amount: 4500 },
+    { date: moment().subtract(2, 'days').format('LL'), amount: 5500 },
+  ]);
+  const [transformedData, setTransformedData] = useState([]);
+
+  // Every time data changes, this useEffect hook will run.
+  useEffect(() => {
+    setTransformedData(transformData(groupBy(data, 'date')));
+  }, [data]);
+
+  // Below is what we get when we use the groupBy function
+  // [
+  // { [moment()]: 2000 },
+  // { [moment().subtract(1, 'days')]: 9500 },
+  // { [moment().subtract(2, 'days')]: 11000 },
+  // ]
+
+  const groupBy = (array, key) =>
+    array.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  // example of what the code above does, in the example, it groups by the number of letters
+  // console.log(groupBy(['one', 'two', 'three'], 'length));
+  // => {3: ["one", "two"], 5: ["three"]}
+
+  // The output currently looks like the code below
+  //   The GROUPED values are--->
+  // {December 27, 2020: Array(1), December 26, 2020: Array(2), December 25, 2020: Array(2)}
+  // December 25, 2020: (2) [{…}, {…}]
+  // December 26, 2020: (2) [{…}, {…}]
+  // December 27, 2020: [{…}]
+
+  // We want the code to look like this
+  // December 25, 2020: 2000
+  // December 26, 2020: 9500
+  // December 27, 2020: 4000
+
   const [gigs, setGigs] = useState([
     {
       description: 'Freelance job with Qazi',
       amount: 499.99,
     },
   ]);
+
+  const getDates = () => transformedData.map((pair) => pair.date);
+  // what we want ---> [date1, date2, date3, date4, date5]
+  // we're using the 0 so that we get '12/28/2020'. The date is the first value in the array. ['12/28/2020'] ---> '12/28/2020'. If we didn't use [0], we would get back an array of arrays
+  const getAmounts = () => transformedData.map((pair) => pair.amount);
+  // Create a new array with the date as the key and value as the sum of the amounts for each date (if it's a grouped date, meaning multiple amounts for a single date, we should see the sum of all of the amounts for that day)
+  const transformData = (groupedData) => {
+    const transformedArray = [];
+
+    Object.entries(groupedData).forEach((entry) => {
+      // Entry[1] is an array of objects that holds all of the amounts (entry[0] holds the date).
+      const total = entry[1].reduce((total, pair) => total + pair.amount, 0);
+      transformedArray.push({ date: entry[0], amount: total });
+    });
+    return transformedArray;
+  };
+
+  console.log('DEBUG-->', data);
+  console.log('Dates--->', getDates());
+  console.log('Amounts--->', getAmounts());
+  console.log(
+    'The GROUPED values are--->',
+    Object.entries(groupBy(data, 'date'))
+  );
+  console.log(
+    'The Total grouped value 👽--->',
+    transformData(groupBy(data, 'date'))
+  );
 
   useEffect(() => {
     setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
@@ -42,6 +116,46 @@ const App = () => {
     <SafeAreaView>
       <View>
         <Text style={styles.firstLabel}>Hello World! 💫</Text>
+      </View>
+
+      <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: getDates(),
+            datasets: [
+              {
+                data: getAmounts(),
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: null, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
       </View>
 
       <Text>Total Income: ${total}</Text>
@@ -66,8 +180,8 @@ const App = () => {
         title="Add Gig 🚀"
       />
 
-      {gigs.map((gig) => (
-        <View>
+      {gigs.map((gig, idx) => (
+        <View key={idx}>
           <Text>{gig.description}</Text>
           <Text>{gig.amount}</Text>
         </View>
---
+++ README.md
@@ -0,0 +1,18 @@
+Income Tracker App
+created with React Native
+
+Video Sources for the Financial Tracker App
+
+1. Clever Programmers: (Let's build an app with REACT NATIVE!)
+   Link: https://www.youtube.com/watch?v=hggGvwA_tcc
+2. Clever Programmers: Build an Income Tracking app with REACT NATIVE!
+   Link: https://www.youtube.com/watch?v=OojrxtudMKM&t=402s&ab_channel=CleverProgrammer
+
+What I learned:
+
+1. How to use interact with state by using useState.
+2. How to style the app using native's stylesheet.
+3. How to use the Text, View, SafeAreaView, TextInput, and Button components.
+4. How to use moments to get and manipulate dates.
+5. How to use the react hook useEffect to tell the app what to do after each render.
+6. Building a chart to track data
