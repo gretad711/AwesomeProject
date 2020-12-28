27291318d87f02ba07a5c2af86992a3341bc89ff
Greta Dakers <gretad711@msn.com>
2020-12-27 19:22:17 -0600

    AwesomeProject

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
+import Todo from './Todo';
 
-export default function App() {
+const App = () => {
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const [gigs, setGigs] = useState([
+    {
+      description: 'Freelance job with Qazi',
+      amount: 499.99,
+    },
+  ]);
+
+  const addGig = () => {
+    setGigs([
+      ...gigs,
+      {
+        description: description,
+        amount: amount,
+      },
+    ]);
+    setDescription('');
+    setAmount('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={styles.firstLabel}>Hello World! 💫</Text>
+      </View>
+
+      <TextInput
+        style={styles.input}
+        value={description}
+        keyboardType="numeric"
+        placeholder="Enter a description"
+        onChangeText={(text) => setDescription(text)}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={amount}
+        keyboardType="numeric"
+        placeholder="Enter the amount you made in USD ($)"
+        onChangeText={(text) => setAmount(text)}
+      />
+      <Button onPress={addGig} title="Add Gig 🚀" />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    margin: 20,
+    padding: 20,
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  firstLabel: {
+    backgroundColor: 'red',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
+
+export default App;
